d0ca8b6790682b2734d42deee96b6fe9e36dc19c
Parminder Singh <parminder.s@flipkart.com>
2018-06-14 19:51:59 +0530

    Raphael

--- src/js/FloorMap.jsx
+++ src/js/FloorMap.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 import floorPlan from './common/floorPlan';
 
+const textStyle = {
+    fontSize: '80px',
+    color: '#000',
+    zIndex: 1000,
+    align: 'center',
+}
+
 function initCanvas(elm, background = '#dcdcdc') {
     return origami(elm)
         .background(background);
@@ -16,19 +23,47 @@ function createFloorCanvas(canvas, floorPlan) {
         const entityDimensions = dimensions[key];
         const entityStyle = fillColor[key]
 
+        if (key === 'paths') {
+            createPaths(canvas, entity);
+        }
+
         entity.forEach(block => {
             const width = block.width || entityDimensions[0];
             const height = block.height || entityDimensions[1];
             const bgColor = block.color || entityStyle;
 
+            canvas.setStart();
             canvas
-                .rect(+block.x, +block.y, width, height)
-                .attr({
-                    fill: bgColor,
-                    stroke: '#000'
+            .rect(+block.x, +block.y, width, height)
+            .attr({
+                fill: bgColor,
+                stroke: '#000'
                 });
+            canvas.text(parseInt(block.x) + parseInt(width) / 2, parseInt(block.y) + parseInt(height) / 2, `${block.id || ''}`)
+            .attr({ fill: '#000', "font-size": 15 });
+            canvas.setFinish();
         });
     }
+}
+
+function createPaths(canvas, entity) {
+    entity.forEach((block) => {
+        const x1 = block.x1;
+        const x2 = block.x2;
+        const y1 = block.y1;
+        const y2 = block.y2;
+
+        canvas.path(`M${x1} ${y1}L${x2} ${y2}Z`)
+            .attr({
+                'stroke': '#c83349',
+                'stroke-dasharray': '.',
+                'stroke-width': 5
+            });
+            
+        // .text(`${x1}, ${y1}`, x1, y1, textStyle)
+        // .text(`${x2}, ${y2}`, x2, y2, textStyle)
+
+    });
 }
 
 export default class FloorMap extends React.PureComponent {
@@ -40,7 +75,7 @@ export default class FloorMap extends React.PureComponent {
     componentDidMount() {
         // const canvas = initCanvas(this.canvasRef.current);
 
-        const canvas = Raphael(0, 0, 3000, 4000);
+        const canvas = Raphael(0, 0, 3335, 4040);
         createFloorCanvas(canvas, floorPlan);
     }
 
