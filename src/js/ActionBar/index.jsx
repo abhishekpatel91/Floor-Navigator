e14a9feaa3bcb3f12fc9fc48aea7e8c844e1e770
Parminder Singh <parminder.s@flipkart.com>
2018-06-15 00:23:54 +0530

    Changes

--- src/js/ActionBar/index.jsx
+++ src/js/ActionBar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { VelocityComponent, VelocityTransitionGroup } from 'velocity-react';
+import { VelocityComponent } from 'velocity-react';
 
 import './index.scss';
 
